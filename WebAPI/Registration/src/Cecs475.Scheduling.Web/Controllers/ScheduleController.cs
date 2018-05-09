3cb9861326ca71969835991130d00d94c983f15b
ajaykc7 <azay.kc137@gmail.com>
2018-05-09 12:14:54 -0700

    Get function tested, JSON as class added

--- WebAPI/Registration/src/Cecs475.Scheduling.Web/Controllers/ScheduleController.cs
+++ WebAPI/Registration/src/Cecs475.Scheduling.Web/Controllers/ScheduleController.cs
@@ -8,6 +8,21 @@ using System.Web.Http;
 
 namespace Cecs475.Scheduling.Web.Controllers
 {
+    public class SemesterTermDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static SemesterTermDto From(Model.SemesterTerm term)
+        {
+            return new SemesterTermDto
+            {
+                Id = term.Id,
+                Name = term.Name
+            };
+        }
+    }
+
     [RoutePrefix("api/schedule")]
     public class ScheduleController : ApiController
     {
@@ -24,23 +39,39 @@ namespace Cecs475.Scheduling.Web.Controllers
         [Route("{year}/{term}")]
         public IEnumerable<CourseSectionDto> GetCourseSection(String year, String term)
         {
+            String termName = term + " "+year;
+            var semesterTerm = mContext.SemesterTerms.Where(c => c.Name == termName).SingleOrDefault();
 
-            var semesterTerm = mContext.SemesterTerms.Where(c => c.Name == year).SingleOrDefault();
-
-            if (semesterTerm == null)
-            {
-                throw new HttpResponseException(Request.CreateErrorResponse(
-                HttpStatusCode.NotFound, $"Invalid semester found"));
-            }
-            return IEnumerable<CourseSectionDto.From(semesterTerm.CourseSections)>;
+            return GetCourseSection(semesterTerm);
         }
 
         [HttpGet]
-        [Route("{id:int")]
+        [Route("{id:int}")]
         public IEnumerable<CourseSectionDto> GetCourseSection(int id)
         {
             var semesterTerm = mContext.SemesterTerms.Where(s => s.Id == id).SingleOrDefault();
 
+            return GetCourseSection(semesterTerm);
+
+        }
+
+        [HttpGet]
+        [Route("{terms}")]
+        public IEnumerable<SemesterTermDto> GetSemesterTerms()
+        {
+            var terms = mContext.SemesterTerms.ToList();
+
+            return terms.Select(SemesterTermDto.From);
+        }
+
+        private IEnumerable<CourseSectionDto> GetCourseSection(Model.SemesterTerm term)
+        {
+            if (term == null)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(
+                HttpStatusCode.NotFound, $"Invalid semester found"));
+            }
+            return term.CourseSections.Select(CourseSectionDto.From);
         }
     }
 }
